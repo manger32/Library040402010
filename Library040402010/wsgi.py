41c14fe5fe58a6e1ea025b7dad6f02777015b13f
german <jimmorrisonhotellizardking@gmail.com>
2023-07-17 18:25:32 +0300

    New Project Library!

---
+++ Library040402010/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Library040402010 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Library040402010.settings')
+
+application = get_wsgi_application()
