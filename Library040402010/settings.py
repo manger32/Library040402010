d4166213f84d011649bab6cc99c4dad318a70882
german <jimmorrisonhotellizardking@gmail.com>
2023-09-01 10:18:39 +0300

    language

--- Library040402010/settings.py
+++ Library040402010/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'Ru-ru'
 
 TIME_ZONE = 'UTC'
 
